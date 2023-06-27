e71845258e52677ac1d2b27bfb233a2f5f016158
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-27 23:00:25 +0300

    creating gallery page with image slider

---
+++ app/gallery/page.js
@@ -0,0 +1,8 @@
+export default function Gallery() {
+  return (
+    <div id="gallery" className="max-w-[1240px] mx-auto">
+      <h1 className="text-2xl font-bold text-center p-4">Gallery</h1>
+      <div className="relative flex justify-center p-4"></div>
+    </div>
+  );
+}
