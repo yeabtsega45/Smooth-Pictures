12196e7f29b71b0f14bd74edd60f59f71e054f4f
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-27 20:15:47 +0300

    creating navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div
+      style={{ backgroundColor: `${color}` }}
+      className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
+    >
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+        <Link href="/">
+          <h1 style={{ color: `${textColor}` }} className="font-bold text-4xl">
+            Captur
+          </h1>
+        </Link>
+        <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
+          <li className="p-4">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/#gallery">Gallery</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/work">Work</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ export default function Home() {
       {/* Overlay */}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
       <div className="p-5 text-white z-[2] mt-[-10rem]">
-        {/* <h2 className="text-5xl font-bold">{heading}</h2> */}
-        {/* <p className="py-5 text-xl">{message}</p> */}
+        <h2 className="text-5xl font-bold">Smooth Pictures</h2>
+        <p className="py-5 text-xl">I capture moments and keep them alive</p>
         <button className="px-8 py-2 border">Book</button>
       </div>
     </div>
