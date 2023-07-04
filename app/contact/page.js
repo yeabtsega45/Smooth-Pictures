6f8414023d657cd3ed4f031c7a30d5e4a8d88e28
yeabtsega45 <yabtsega2022@gmail.com>
2023-07-04 20:15:59 +0300

    styling the app

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,32 +1,35 @@
 export default function Contact() {
   return (
-    <div id="contact" className="bg-black/70 h-screen pt-14">
-      <h1 className="text-4xl font-bold text-center p-4">Lets work together</h1>
-      <form className="max-w-[600px] m-auto">
-        <div className="grid grid-cols-2 gap-2">
+    <div
+      id="contact"
+      className="flex flex-col justify-center items-center m-auto bg-black/70 h-screen pt-[80px]"
+    >
+      <h1 className="text-3xl font-bold text-center">Lets work together</h1>
+      <form className="md:max-w-[600px] max-w-[350px] h-[90%] m-auto">
+        <div className="grid md:grid-cols-2 gap-2">
           <input
-            className="border shadow-lg p-3"
+            className="border shadow-lg p-2"
             type="text"
             placeholder="Name"
           />
           <input
-            className="border shadow-lg p-3"
+            className="border shadow-lg p-2"
             type="email"
             placeholder="Email"
           />
         </div>
         <input
-          className="border shadow-lg p-3 w-full my-2"
+          className="border shadow-lg p-2 w-full my-2"
           type="text"
           placeholder="Subject"
         />
         <textarea
-          className="border shadow-lg p-3 w-full"
+          className="border shadow-lg p-2 w-full"
           cols="30"
           rows="10"
           placeholder="Message"
         ></textarea>
-        <button className="border bg-blue-700 text-white shadow-lg p-3 w-full mt-2">
+        <button className="border bg-blue-700 text-white shadow-lg p-2 w-full mt-2">
           Submit
         </button>
       </form>
--- app/work/page.js
+++ app/work/page.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Work() {
   return (
-    <div id="work" className="text-center bg-black/70 h-screen pt-14">
-      <h1 className="font-bold text-4xl p-4">Travel Photos</h1>
+    <div id="work" className="text-center bg-black/70 h-screen py-14">
+      <h1 className="font-bold text-3xl p-4 mt-3">Travel Photos</h1>
       <div className="grid grid-rows-none md:grid-cols-5 p-4 gap-4">
-        <div className="w-full h-full col-span-2 md:col-span-3 row-span-2">
+        <div className="md:w-[90%] md:h-[90%] w-[80%] h-[80%] col-span-2 md:col-span-3 row-span-2">
           <Image
             src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
             alt="/"
