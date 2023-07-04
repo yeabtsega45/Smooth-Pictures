52ed50969329cabc2469581e746cb82191fce875
yeabtsega45 <yabtsega2022@gmail.com>
2023-07-04 21:10:41 +0300

    styling the app

--- app/work/page.js
+++ app/work/page.js
@@ -4,8 +4,8 @@ export default function Work() {
   return (
     <div id="work" className="text-center bg-black/70 h-screen py-14">
       <h1 className="font-bold text-3xl p-4 mt-3">Travel Photos</h1>
-      <div className="grid grid-rows-none md:grid-cols-5 p-4 gap-4">
-        <div className="md:w-[90%] md:h-[90%] w-[80%] h-[80%] col-span-2 md:col-span-3 row-span-2">
+      <div className="grid grid-rows-none md:grid-cols-5 p-4 gap-4 w-[90%] h-[90%] m-auto">
+        <div className="w-[100%] h-[100%] col-span-2 md:col-span-3 row-span-2">
           <Image
             src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
             alt="/"
