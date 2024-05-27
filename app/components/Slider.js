a4a03ef5321b7017364f0e759241badb3d878b67
yeabtsega45 <yabtsega2022@gmail.com>
2024-05-27 12:08:04 +0300

    update styling

--- app/components/Slider.js
+++ app/components/Slider.js
@@ -19,42 +19,39 @@ const Slider = ({ slides }) => {
   }
 
   return (
-    <div id="gallery" className="max-w-[1240px] mx-auto">
-      <h1 className="text-2xl font-bold text-center p-4">Gallery</h1>
-      <div className="relative flex justify-center p-4">
-        {SliderData.map((slide, index) => {
-          return (
-            <div
-              key={index}
-              className={
-                index === current
-                  ? "opacity-[1] ease-in duration-1000"
-                  : "opacity-0"
-              }
-            >
-              <FaArrowCircleLeft
-                onClick={prevSlide}
-                className="absolute top-[50%] left-[30px] text-white/70 cursor-pointer select-none z-[2]"
-                size={50}
+    <div className="relative flex justify-center p-4">
+      {SliderData.map((slide, index) => {
+        return (
+          <div
+            key={index}
+            className={
+              index === current
+                ? "opacity-[1] ease-in duration-1000"
+                : "opacity-0"
+            }
+          >
+            <FaArrowCircleLeft
+              onClick={prevSlide}
+              className="absolute top-[50%] left-[30px] text-white/70 cursor-pointer select-none z-[2]"
+              size={50}
+            />
+            {index === current && (
+              <Image
+                src={slide.image}
+                alt="/"
+                width="1440"
+                height="600"
+                objectFit="cover"
               />
-              {index === current && (
-                <Image
-                  src={slide.image}
-                  alt="/"
-                  width="1440"
-                  height="600"
-                  objectFit="cover"
-                />
-              )}
-              <FaArrowCircleRight
-                onClick={nextSlide}
-                className="absolute top-[50%] right-[30px] text-white/70 cursor-pointer select-none z-[2]"
-                size={50}
-              />
-            </div>
-          );
-        })}
-      </div>
+            )}
+            <FaArrowCircleRight
+              onClick={nextSlide}
+              className="absolute top-[50%] right-[30px] text-white/70 cursor-pointer select-none z-[2]"
+              size={50}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,31 +1,16 @@
 "use client";
 
-import Image from "next/image";
 import React from "react";
 import { SliderData } from "../components/SliderData";
-import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Slider from "../components/Slider";
 
 export default function Gallery({ slides }) {
   return (
     <div id="gallery" className="z-[2] h-screen pt-14">
       <h1 className="text-3xl font-bold text-center p-4 mt-3">Gallery</h1>
       <div className="relative flex justify-center items-center m-auto p-4 w-[450px] md:w-[650px]">
-        <Carousel>
-          {SliderData.map((slide, index) => {
-            return (
-              <div key={index}>
-                <Image
-                  src={slide.image}
-                  alt="/"
-                  width="1440"
-                  height="600"
-                  objectFit="cover"
-                />
-              </div>
-            );
-          })}
-        </Carousel>
+        <Slider slides={SliderData} />
       </div>
     </div>
   );
